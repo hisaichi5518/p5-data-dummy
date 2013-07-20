c3ab7063c2dd15cc9f5c3d351c0cfbff07835150
hisaichi5518 <hisada.kazuki@gmail.com>
2013-07-20 15:59:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Dummy - It's new $module
+
+# SYNOPSIS
+
+    use Data::Dummy;
+
+# DESCRIPTION
+
+Data::Dummy is ...
+
+# LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hisaichi5518 <hisada.kazuki@gmail.com>
---
+++ lib/Data/Dummy.pm
@@ -0,0 +1,39 @@
+package Data::Dummy;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Dummy - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Dummy;
+
+=head1 DESCRIPTION
+
+Data::Dummy is ...
+
+=head1 LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hisaichi5518 E<lt>hisada.kazuki@gmail.comE<gt>
+
+=cut
+
